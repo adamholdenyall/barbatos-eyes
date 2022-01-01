378446ca25bdcdd73287330ffbbb15f90a8de939
adamholdenyall <adammholden@gmail.com>
2022-01-01 17:57:12 -0600

    Trimmed code, added write/log toggles

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 
 import board
 from digitalio import DigitalInOut, Direction, Pull
-from analogio import AnalogOut, AnalogIn
 import touchio
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keycode import Keycode
 import adafruit_dotstar as dotstar
 import time
 import neopixel
+
+writePixels = False;
+logPixels = True;
 
 # One pixel connected internally!
 dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
@@ -17,12 +17,6 @@ dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
 # Built in red LED
 led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
-
-# Analog input on D0
-analog1in = AnalogIn(board.D0)
-
-# Analog output on D1
-aout = AnalogOut(board.D1)
 
 # Digital input with pullup on D2
 button = DigitalInOut(board.D2)
@@ -36,14 +30,7 @@ touch = touchio.TouchIn(board.D3)
 NUMPIXELS = 16
 neopixels = neopixel.NeoPixel(board.D4, NUMPIXELS, brightness=0.2, auto_write=False)
 
-# Used if we do HID output, see below
-kbd = Keyboard()
-
 ######################### HELPERS ##############################
-
-# Helper to convert analog input to voltage
-def getVoltage(pin):
-    return (pin.value * 3.3) / 65536
 
 # Helper to give us a nice color swirl
 def wheel(pos):
@@ -72,35 +59,28 @@ while True:
   # spin internal LED around! autoshow is on
   colorString = ''
   dotColor = wheel(i & 255)
-  dot[0] = dotColor
-  colorString += getColorString(dotColor,' ')
+  if(writePixels):
+    dot[0] = dotColor
+  if(logPixels):
+    colorString += getColorString(dotColor,' ')
 
   # also make the neopixels swirl around
   for p in range(NUMPIXELS):
       idx = int ((p * 256 / NUMPIXELS) + i)
       npColor = wheel(idx & 255)
-      neopixels[p] = npColor
-      colorString += getColorString(npColor,' ')
-  neopixels.show()
-
-  print(colorString)
-
-  # set analog output to 0-3.3V (0-65535 in increments)
-  aout.value = i * 256
-
-  # Read analog voltage on D0
-  print("D0: %0.2f" % getVoltage(analog1in))
+      if(writePixels):
+        neopixels[p] = npColor
+      if(logPixels):
+        colorString += getColorString(npColor,' ')
+  if(writePixels):
+    neopixels.show()
+  if(logPixels):
+    print(colorString)
   
   # use D3 as capacitive touch to turn on internal LED
   if touch.value:
       print("D3 touched!")
   led.value = touch.value
-
-  if not button.value:
-      print("Button on D2 pressed!")
-      # optional! uncomment below & save to have it sent a keypress
-      #kbd.press(Keycode.A)
-      #kbd.release_all()
 
   i = (i+1) % 256  # run from 0 to 255
   time.sleep(0.01) # make bigger to slow down
